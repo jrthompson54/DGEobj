845f17f5b5c9392ba728bb81f8f55aece093e281
John Thompson <john.thompson@bms.com>
2017-04-19 17:47:51 -0400

    added annotateDGEobj function (needs testing)

--- R/index.R
+++ R/index.R
@@ -51,3 +51,84 @@ indexType <- function(dgeObj, type, moreAttr){
         return(NULL)
     }
 }
+
+
+### Function annotateDGEobj ###
+#' Function annotateDGEobj
+#'
+#' Read an Omicsoft Registration file and attach attributes to the DGEobj.
+#'
+#' Omicsoft registration files contain a [SampleSet] section that contains
+#' metadata about a project.  These are key/value pairs separated by an equals
+#' sign.  The keys parameter specifies which key we want to capture as
+#' attributes on the DGEobj.
+#'
+#' As long as you provide a text file with key/value pairs separated by equals
+#' sign, this function should work.
+#'
+#' @author John Thompson, \email{john.thompson@@bms.com}
+#' @keywords RNA-Seq, DGEobj, annotation, attributes
+#'
+#' @param dgeObj  A class dgeObj created by function initDGEobj
+#' @param regfile An omicsoft registration file (text not Excel!)
+#' @param keys A list of keys to look for and transfer to the DGEobj
+#'
+#' @return A DGEobj annotated with attributes.
+#'
+#' @examples
+#'    MyDgeObj <- annotateDGEobj(DGEobj, regfile)
+#'
+#' @import magrittr assertthat
+#'
+#' @export
+annotateDGEobj <- function(dgeObj, regfile,
+                           keys = list("ID",
+                                       "BMS_PID",
+                                       "Title",
+                                       "Description",
+                                       "Keywords",
+                                       "Business Unit",
+                                       "Functional Area",
+                                       "Disease",
+                                       "Vendor",
+                                       "PlatformType",
+                                       "Technology",
+                                       "LibraryPrep",
+                                       "Organism",
+                                       "Tissue",
+                                       "AlignmentReference",
+                                       "GeneModel",
+                                       "TBio_Owner",
+                                       "TA_Owner",
+                                       "LoadData",
+                                       "ReadLength",
+                                       "ReadType",
+                                       "Pipeline",
+                                       "AlignmentAlgorithm",
+                                       "ScriptID"
+                           )){
+
+
+    # Read lines, stripping quotes
+    regdat <- read.delim(regfile, sep="\t",
+                         quote = "\"",
+                         stringsAsFactors = FALSE)
+    #just first column
+    regdat <- regdat[,1, drop=FALSE]
+    colnames(regdat) <- "pair"
+    #just lines with equals signs
+    regdat <- filter(regdat, grepl("=", pair), !grepl("Parameters.", pair))
+    #now split into key value pairs
+    regdat <- strsplit(regdat$pair, "=") %>% as.data.frame() %>% t
+    colnames(regdat) <- c("key", "value")
+
+    MyAttribs <- list()
+    for (i in 1:nrows(regdat))
+        if (regdat$key[i] %in% keys)
+            MyAttribs[regdat$key[i]] <- regdat$value[i]
+
+    #now attach the attributes to the DGEobj
+    attributes(dgeObj) <- MyAttribs
+    return(dgeObj)
+}
+
