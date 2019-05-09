970906316635722c941e6db6517903fbea1fe458
John Thompson <john.thompson@bms.com>
2019-05-09 19:31:34 -0400

    add function rmItems

--- R/rmItem.R
+++ R/rmItem.R
@@ -39,3 +39,45 @@ rmItem <- function(dgeObj, itemName){
 
     return(dgeObj)
 }
+
+### Function rmItems ###
+#' Function rmItems
+#'
+#' Removes a vector or list of named data items from a DGEobj
+#'
+#' @author John Thompson, \email{john.thompson@@bms.com}
+#' @keywords RNA-Seq, DGEobj
+#'
+#' @param dgeObj  A class dgeObj created by function initDGEobj
+#' @param items Either a character vector of item names or a numeric index of items to
+#'   remove from the dgeObj.  Use inventory(dgeobj) to view the indexes of items.
+#'
+#' @return An updated DGEobj
+#'
+#' @examples
+#'    MyDgeObj <- rmItems(MyDgeObj, c("designMatrix", "designMatrix_Elist"))
+#'
+#' @importFrom assertthat assert_that
+#'
+#' @export
+rmItems <- function(dgeObj, items){
+    # browser()
+    assertthat::assert_that(!missing(dgeObj),
+                            !missing(items),
+                            "DGEobj" %in% class(dgeObj))
+
+    #let's tolerate lists too.
+    if ("list" %in% class(items)) items <- unlist(items)
+
+    if (any(c("numeric", "integer") %in% class(items)) & max(items) > length(dgeObj))
+        stop("A value in items numeric index is gt items in dgeObj" )
+
+    #convert numeric Index to Names
+    if (any(c("numeric", "integer") %in% class(items))) items <- names(dgeObj)[items]
+
+    #delete the items
+    for (item in items){
+        dgeObj <- rmItem(dgeObj, item)
+    }
+    return(dgeObj)
+}
