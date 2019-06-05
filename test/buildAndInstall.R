66610f8ed1907c448b4710a1d74c59d817100ff5
John Thompson <john.thompson@bms.com>
2019-06-05 12:00:57 -0400

    v0.9.40 no changes; Package Manager test

--- test/buildAndInstall.R
+++ test/buildAndInstall.R
@@ -8,7 +8,7 @@ document()
 load_all()
 # build_vignettes()
 build()
-pkg = "~/R/lib/pkgsrc/DGEobj_0.9.39.tar.gz"
+pkg = "~/R/lib/pkgsrc/DGEobj_0.9.40.tar.gz"
 install.packages(pkg, repos=NULL, type="source")
 setwd(x)
 
